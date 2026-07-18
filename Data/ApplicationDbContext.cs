05e456ef957beff8762c67fc232a337604b0f104
jydhasan <s1910933181@ru.ac.bd>
2026-07-18 10:35:59 +0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Range(0, 1000000)]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
